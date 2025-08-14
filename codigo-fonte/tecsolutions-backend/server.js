420a6e86daa911fe95a43f4e5132d2fa5024fb87
v4gn32 <vagner83@icloud.com>
2025-08-14 20:15:48 -0300

    doc: adicionado arquivos extras

--- codigo-fonte/tecsolutions-backend/server.js
+++ codigo-fonte/tecsolutions-backend/server.js
@@ -1,8 +1,8 @@
 // tecsolutions-backend/server.js
 import dotenv from "dotenv";
-import app from "./src/app.js"; 
+import app from "./src/app.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}🚀`));
