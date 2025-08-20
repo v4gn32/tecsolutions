36c93b0e6db80edb886a2d3aef94004230f87394
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2025-08-20 20:58:25 -0300

    teste do codigo no pc

--- codigo-fonte/tecsolutions-backend/src/controllers/auth.controller.js
+++ codigo-fonte/tecsolutions-backend/src/controllers/auth.controller.js
@@ -1,44 +1,50 @@
-import prisma from '../config/db.js';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import prisma from "../config/db.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "devsecret";
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await prisma.user.findUnique({ where: { email } });
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).json({ error: 'Credenciais inválidas' });
+    return res.status(401).json({ error: "Credenciais inválidas" });
   }
 
-  const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, {
-    expiresIn: '1d'
-  });
+  const token = jwt.sign(
+    { id: user.id, role: user.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
 
   res.json({
     token,
-    user: { id: user.id, name: user.name, email: user.email, role: user.role }
+    user: { id: user.id, name: user.name, email: user.email, role: user.role },
   });
 };
 
 export const register = async (req, res) => {
-  const { name, email, password, role = 'USER', createdBy = null } = req.body;
+  const { name, email, password, role = "USER", createdBy = null } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await prisma.user.create({
-    data: { name, email, password: hashedPassword, role, createdBy }
+    data: { name, email, password: hashedPassword, role, createdBy },
   });
 
   res.status(201).json({
     id: user.id,
     name: user.name,
     email: user.email,
-    role: user.role
+    role: user.role,
   });
 };
 
 export const profile = async (req, res) => {
   const user = await prisma.user.findUnique({
     where: { id: req.user.id },
-    select: { id: true, name: true, email: true, role: true }
+    select: { id: true, name: true, email: true, role: true },
   });
   res.json(user);
 };
